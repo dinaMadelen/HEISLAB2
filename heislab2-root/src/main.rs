63b0b0943890534db42da9f0b96c8333f12179d1
Kristoffer Onstad Schie <kris.schie@gmail.com>
2025-02-27 17:46:45 +0100

    laget en lib.rs fil

--- heislab2-root/src/main.rs
+++ heislab2-root/src/main.rs
@@ -1,12 +1,6 @@
 // sett kompilator flagg
 #![allow(dead_code, unused_imports)]
 
-//------------------------
-// Initiell konfigurering
-//------------------------
-// Importer moduler og crates
-mod modules;
-// sett opp modul alias
-use modules::udp;
+use heislab2_root::modules;
 
 fn main() {}

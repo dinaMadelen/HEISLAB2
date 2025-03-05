f86f6fc66c5b6cd209cbb3af0ac0236dd2cd087e
Hackerman <kris.schie@gmail.com>
2025-03-05 23:54:30 +0100

    Ryddet div

--- heislab2-root/src/elevator_2.rs
+++ heislab2-root/src/elevator_2.rs
@@ -15,7 +15,7 @@ pub enum ElevatorStatus {
     DoorOpen,
     Error,
     Stop,
-    MovingUp, //kristoffer
+    MovingUp,   //kristoffer
     MovingDown, //kristoffer
 }
 
@@ -76,6 +76,7 @@ pub const DIRN_STOP: u8 = 0;
 pub const DIRN_UP: u8 = 1;
 
 impl Elevator {
+    /// Initializes a instance of the elevator struct (creates a elevator)
     pub fn init(addr: &str, num_floors: u8) -> Result<Elevator> {
         Ok(Self {
             socket: Arc::new(Mutex::new(TcpStream::connect(addr)?)),
@@ -88,36 +89,42 @@ impl Elevator {
         })
     }
 
+    /// Sets the motor direction
     pub fn motor_direction(&self, dirn: u8) {
         let buf = [1, dirn, 0, 0];
         let mut sock = self.socket.lock().unwrap();
         sock.write(&buf).unwrap();
     }
 
+    /// Turns on call buttn light at speficic floor
     pub fn call_button_light(&self, floor: u8, call: u8, on: bool) {
         let buf = [2, call, floor, on as u8];
         let mut sock = self.socket.lock().unwrap();
         sock.write(&buf).unwrap();
     }
 
+    /// Indicates floor
     pub fn floor_indicator(&self, floor: u8) {
         let buf = [3, floor, 0, 0];
         let mut sock = self.socket.lock().unwrap();
         sock.write(&buf).unwrap();
     }
 
+    /// Sets door light
     pub fn door_light(&self, on: bool) {
         let buf = [4, on as u8, 0, 0];
         let mut sock = self.socket.lock().unwrap();
         sock.write(&buf).unwrap();
     }
 
+    /// Sets stop button light
     pub fn stop_button_light(&self, on: bool) {
         let buf = [5, on as u8, 0, 0];
         let mut sock = self.socket.lock().unwrap();
         sock.write(&buf).unwrap();
     }
 
+    /// Sets a call to a given floor
     pub fn call_button(&self, floor: u8, call: u8) -> bool {
         let mut buf = [6, call, floor, 0];
         let mut sock = self.socket.lock().unwrap();
@@ -126,6 +133,8 @@ impl Elevator {
         buf[1] != 0
     }
 
+    /// Queries the elevator system for the current floor, and returns `Some(<floor>)` with the floor number if the system responds to the query.
+    /// If the floor is not known, the function returns `None`      
     pub fn floor_sensor(&self) -> Option<u8> {
         let mut buf = [7, 0, 0, 0];
         let mut sock = self.socket.lock().unwrap();
@@ -138,6 +147,7 @@ impl Elevator {
         }
     }
 
+    /// Sets the stop button
     pub fn stop_button(&self) -> bool {
         let mut buf = [8, 0, 0, 0];
         let mut sock = self.socket.lock().unwrap();
@@ -146,6 +156,7 @@ impl Elevator {
         buf[1] != 0
     }
 
+    /// Sets obstruction
     pub fn obstruction(&self) -> bool {
         let mut buf = [9, 0, 0, 0];
         let mut sock = self.socket.lock().unwrap();
@@ -154,6 +165,7 @@ impl Elevator {
         buf[1] != 0
     }
 
+    /// Add floor to queue
     pub fn add_to_queue(&mut self, floor: u8) {
         if !self.queue.contains(&floor) {
             self.queue.push(floor);
@@ -163,92 +175,101 @@ impl Elevator {
         }
     }
 
-    // Sets current status (Enum Status) for elevator,
+    /// Sets status for elevator
     pub fn set_status(&mut self, status: ElevatorStatus) {
         match status {
             // Floors are read as u8 0 is hall up, 1 hall down, 2 cab
-            ElevatorStatus::Moving => {
-                //HVIS DET ER EN ERROR MÅ VI SE OM DET VAR FORRIGE STATUS DA SKAL VI IKKE GJØRE NOE
-                match self.status {
-                    ElevatorStatus::Moving | ElevatorStatus::Idle => {
-                        self.status = ElevatorStatus::Moving;
-                        let first_item_in_queue = self.queue.first().unwrap();
-                        if *first_item_in_queue < self.current_floor {
-                            self.direction = -1;
-                        } else if *first_item_in_queue > self.current_floor {
-                            self.direction = 1;
-                        }
-                    }
-                    _ => {
-                        //Do Something?
-                    }
+            ElevatorStatus::Moving => self.handle_moving_status(),
+            ElevatorStatus::DoorOpen => self.handle_door_open_status(),
+            ElevatorStatus::Idle => self.handle_idle_status(),
+            ElevatorStatus::Stop => self.handle_stop_status(),
+            ElevatorStatus::Error => self.handle_error_status(),
+            _ => (), // denne må fikses
+        }
+    }
+
+    pub fn handle_moving_status(&mut self) -> () {
+        //HVIS DET ER EN ERROR MÅ VI SE OM DET VAR FORRIGE STATUS DA SKAL VI IKKE GJØRE NOE
+        match self.status {
+            ElevatorStatus::Moving | ElevatorStatus::Idle => {
+                self.status = ElevatorStatus::Moving;
+                let first_item_in_queue = self.queue.first().unwrap();
+                if *first_item_in_queue < self.current_floor {
+                    self.direction = -1;
+                } else if *first_item_in_queue > self.current_floor {
+                    self.direction = 1;
                 }
-                //IMPLEMENT LIGHT FUNCTIONALITY HERE
             }
+            _ => {
+                //Do Something?
+            }
+        }
+        //IMPLEMENT LIGHT FUNCTIONALITY HERE
+    }
 
-            ElevatorStatus::DoorOpen => match self.status {
-                ElevatorStatus::DoorOpen => {
-                    self.status = ElevatorStatus::Idle;
-                }
-                _ => {
-                    self.status = ElevatorStatus::DoorOpen;
-                }
-            },
-
-            ElevatorStatus::Idle => {
+    pub fn handle_door_open_status(&mut self) {
+        match self.status {
+            ElevatorStatus::DoorOpen => {
                 self.status = ElevatorStatus::Idle;
-
-                //SKRUR AV LYSET FOR DER DEN ER
-                if self.direction == -1 {
-                    self.call_button_light(self.current_floor, HALL_UP, false);
-                } else {
-                    self.call_button_light(self.current_floor, HALL_DOWN, false);
-                };
-                self.call_button_light(self.current_floor, CAB, false);
-
-                //SIER DEN IKKE BEVEGER SEG LENGER
-                self.direction = 0;
             }
-
-            ElevatorStatus::Stop => {
-                match self.status {
-                    ElevatorStatus::Stop => {
-                        self.status = ElevatorStatus::Idle;
-                    }
-                    _ => {
-                        // KILL ELEVATOR !?
-                        for f in 0..(self.num_floors) {
-                            for c in 0..3 {
-                                self.call_button_light(f, c, false);
-                            }
-                        }
-
-                        self.motor_direction(DIRN_STOP);
-                        self.status = ElevatorStatus::Stop;
-                        self.queue.clear();
-                        self.print_status();
-                    }
-                }
-            }
-
-            ElevatorStatus::Error => {
-                match self.status {
-                    ElevatorStatus::Error => {
-                        self.status = ElevatorStatus::Idle;
-                    }
-                    _ => {
-                        // KILL ELEVATOR !
-
-                        self.motor_direction(DIRN_STOP);
-                        self.status = ElevatorStatus::Error;
-                        self.queue.clear();
-                        self.print_status();
-                    }
-                }
+            _ => {
+                self.status = ElevatorStatus::DoorOpen;
             }
         }
     }
-    
+
+    pub fn handle_idle_status(&mut self) {
+        self.status = ElevatorStatus::Idle;
+
+        //SKRUR AV LYSET FOR DER DEN ER
+        if self.direction == -1 {
+            self.call_button_light(self.current_floor, HALL_UP, false);
+        } else {
+            self.call_button_light(self.current_floor, HALL_DOWN, false);
+        };
+        self.call_button_light(self.current_floor, CAB, false);
+
+        //SIER DEN IKKE BEVEGER SEG LENGER
+        self.direction = 0;
+    }
+
+    pub fn handle_stop_status(&mut self) {
+        match self.status {
+            ElevatorStatus::Stop => {
+                self.status = ElevatorStatus::Idle;
+            }
+            _ => {
+                // KILL ELEVATOR !?
+                for f in 0..(self.num_floors) {
+                    for c in 0..3 {
+                        self.call_button_light(f, c, false);
+                    }
+                }
+
+                self.motor_direction(DIRN_STOP);
+                self.status = ElevatorStatus::Stop;
+                self.queue.clear();
+                self.print_status();
+            }
+        }
+    }
+
+    pub fn handle_error_status(&mut self) {
+        match self.status {
+            ElevatorStatus::Error => {
+                self.status = ElevatorStatus::Idle;
+            }
+            _ => {
+                // KILL ELEVATOR !
+
+                self.motor_direction(DIRN_STOP);
+                self.status = ElevatorStatus::Error;
+                self.queue.clear();
+                self.print_status();
+            }
+        }
+    }
+
     pub fn sort_queue(&self) -> Vec<u8> {
         let mut sorted_queue = self.queue.clone();
         let (mut non_negative, mut negative): (Vec<u8>, Vec<u8>) =

9bc11244c6e4a45983cb6ea24c9c5669c0c44025
dinaMadelen <dmshegna@gmail.com>
2025-03-06 17:46:06 +0100

    test dead but in there

--- heislab2-root/src/modules/elevator.rs
+++ heislab2-root/src/modules/elevator.rs
@@ -350,43 +350,43 @@ use elev::Status;
 #[cfg(test)] // https://doc.rust-lang.org/book/ch11-03-test-organization.html Run tests with "cargo test"
 mod tests {
     use super::*;
-    
-    let elev_num_floors = 4;
-    let mut elevator = e::Elevator::init("localhost:15657", elev_num_floors).expect("Failed to initialize elevator");
-    
-    println!("Elevator started:\n{:#?}", elevator);
-
-    let poll_period = Duration::from_millis(25);
-
-    let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
-    }
-
-    let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
-    }
-
-    let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
-    }
-
-    let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
-    {
-        let elevator = elevator.clone();
-        spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
-    }
-
-    let mut dirn = e::DIRN_DOWN;
 
     #[test]
     //Function for testing the set status mod
     fn test_set_status() {
+        let elev_num_floors = 4;
+        let mut elevator = e::Elevator::init("localhost:15657", elev_num_floors).expect("Failed to initialize elevator");
+        
+        println!("Elevator started:\n{:#?}", elevator);
+        
+        let poll_period = Duration::from_millis(25);
+
+        let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
+        }
+
+        let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
+        }
+
+        let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
+        }
+
+        let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
+        }
+
+        let mut dirn = e::DIRN_DOWN;
+
 
         //IDLE TEST
         elevator.set_status(Status::Idle);
@@ -427,12 +427,45 @@ mod tests {
 
         println!("Test: STOP OK");
 
-    }
+        }
 
     #[test]
     fn test_go_to_floor() {
         elevator.add_to_queue(3);
         elevator.go_next_floor();
+        let elev_num_floors = 4;
+        let mut elevator = e::Elevator::init("localhost:15657", elev_num_floors).expect("Failed to initialize elevator");
+        
+        println!("Elevator started:\n{:#?}", elevator);
+        
+        let poll_period = Duration::from_millis(25);
+
+        let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
+        }
+
+        let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
+        }
+
+        let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
+        }
+
+        let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
+        {
+            let elevator = elevator.clone();
+            spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
+        }
+
+        let mut dirn = e::DIRN_DOWN;
+
 
         let seconds = Duration::from_secs(5);
         let start = SystemTime::now();

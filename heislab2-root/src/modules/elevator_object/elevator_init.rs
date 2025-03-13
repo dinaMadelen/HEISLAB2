edd941b8d4d748915f4ae512b159204a75411a8c
dinaMadelen <dmshegna@gmail.com>
2025-03-13 17:03:27 +0100

    added elevator movement

--- heislab2-root/src/modules/elevator_object/elevator_init.rs
+++ heislab2-root/src/modules/elevator_object/elevator_init.rs
@@ -102,55 +102,6 @@ impl Elevator {
         buf[1] != 0
     }
 
-
-    // Moves to next floor, if empty queue, set status to idle. If !(moving  idle), do nothing
-    pub fn go_next_floor(&mut self) {
-        if ((self.status == Status::Moving) | (self.status == Status::Idle)){
-            if let Some(next_floor) = self.queue.first() {
-                if *next_floor > self.current_floor {
-                    self.set_status(Status::Moving);
-                    self.motor_direction(DIRN_UP);
-                    //self.current_floor += 1;
-                    
-                } else if *next_floor < self.current_floor {
-                    self.set_status(Status::Moving);
-                    self.motor_direction(DIRN_DOWN);
-                    //self.current_floor -= 1;
-                    
-                } else if *next_floor == self.current_floor{
-                    self.set_status(Status::Idle);
-                    self.motor_direction(DIRN_STOP);
-                    self.queue.remove(0);
-                    
-                    self.door_open_sequence();
-
-            
-                }
-            } else {
-                self.set_status(Status::Idle);
-                self.motor_direction(DIRN_STOP);
-            }
-        } else {
-            self.motor_direction(DIRN_STOP);
-        }
-    }
-
-
-    
-    //MIDLERTIDIG FUNKSJON
-    pub fn door_open_sequence(&mut self) {
-        self.set_status(Status::DoorOpen);
-
-        let handle = thread::spawn(|| {
-            thread::sleep(Duration::from_secs(2)); // Sleep for 2 seconds
-            
-            println!("Thread woke up!");
-        });
-
-        //handle.join().unwrap(); // Wait for the thread to finish
-        self.set_status(Status::DoorOpen);
-        self.go_next_floor();
-    }
 }
 
 
---
+++ heislab2-root/src/modules/elevator_object/elevator_movement.rs
@@ -0,0 +1,47 @@
+
+impl Elevator{
+    pub fn door_open_sequence(&mut self) {
+        self.set_status(Status::DoorOpen);
+
+        let handle = thread::spawn(|| {
+            thread::sleep(Duration::from_secs(2)); // Sleep for 2 seconds
+            
+            println!("Thread woke up!");
+        });
+
+        //handle.join().unwrap(); // Wait for the thread to finish
+        self.set_status(Status::DoorOpen);
+        self.go_next_floor();
+    }
+
+    pub fn go_next_floor(&mut self) {
+        if ((self.status == Status::Moving) | (self.status == Status::Idle)){
+            if let Some(next_floor) = self.queue.first() {
+                if *next_floor > self.current_floor {
+                    self.set_status(Status::Moving);
+                    self.motor_direction(DIRN_UP);
+                    //self.current_floor += 1;
+                    
+                } else if *next_floor < self.current_floor {
+                    self.set_status(Status::Moving);
+                    self.motor_direction(DIRN_DOWN);
+                    //self.current_floor -= 1;
+                    
+                } else if *next_floor == self.current_floor{
+                    self.set_status(Status::Idle);
+                    self.motor_direction(DIRN_STOP);
+                    self.queue.remove(0);
+                    
+                    self.door_open_sequence();
+
+            
+                }
+            } else {
+                self.set_status(Status::Idle);
+                self.motor_direction(DIRN_STOP);
+            }
+        } else {
+            self.motor_direction(DIRN_STOP);
+        }
+    }
+}

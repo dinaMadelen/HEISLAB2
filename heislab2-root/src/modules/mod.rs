19ac2a1429c7768153a1e7818f074d01c252cabe
dinaMadelen <dmshegna@gmail.com>
2025-03-13 17:32:57 +0100

    updated mod with elevator funcs

--- heislab2-root/src/modules/mod.rs
+++ heislab2-root/src/modules/mod.rs
@@ -1,7 +1,11 @@
 pub mod udp;
-pub mod elevator;
 pub mod master;
 pub mod slave;
 pub mod system_init;
 pub mod poll;
 pub mod light_control;
+
+pub mod elevator_init;
+pub mod elevator_movement;
+pub mod elevator_queue_handling;
+pub mod elevator_status_functions;

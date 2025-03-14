9c62581589d65022a939314b93d4dbc72cc1024d
dinaMadelen <dmshegna@gmail.com>
2025-03-14 13:43:51 +0100

    added new main

--- heislab2-root/src/main.rs
+++ heislab2-root/src/main.rs
@@ -1,50 +1,49 @@
-// sett kompilator flagg
-#![allow(dead_code, unused_imports)]
-use heislab2_root::modules;
-
 use std::thread::*;
 use std::time::*;
-use elevio::elev;
 use crossbeam_channel as cbc;
 
-use driver_rust::elevio;
-use driver_rust::elevio::elev as e;
-use elev::Status;
+use driver_rust::modules::elevator_object::*;
+use alias_lib::{HALL_DOWN, HALL_UP, CAB, DIRN_DOWN, DIRN_UP, DIRN_STOP};
+use elevator_init::Elevator;
+use elevator_status_functions::Status;
+use driver_rust::modules::order_object::order_init::Order;
 
+
+// THIS IS SUPPOSED TO BE A SINGLE ELEVATOR MAIN THAT CAN RUN IN ONE THREAD
 
 
 fn main() -> std::io::Result<()> {
     let elev_num_floors = 4;
-    let mut elevator = e::Elevator::init("localhost:15657", elev_num_floors)?;
+    let mut elevator = Elevator::init("localhost:15657", elev_num_floors)?;
     println!("Elevator started:\n{:#?}", elevator);
 
     let poll_period = Duration::from_millis(25);
 
-    let (call_button_tx, call_button_rx) = cbc::unbounded::<elevio::poll::CallButton>();
+    let (call_button_tx, call_button_rx) = cbc::unbounded::<poll::CallButton>();
     {
         let elevator = elevator.clone();
-        spawn(move || elevio::poll::call_buttons(elevator, call_button_tx, poll_period));
+        spawn(move || poll::call_buttons(elevator, call_button_tx, poll_period));
     }
 
     let (floor_sensor_tx, floor_sensor_rx) = cbc::unbounded::<u8>();
     {
         let elevator = elevator.clone();
-        spawn(move || elevio::poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
+        spawn(move || poll::floor_sensor(elevator, floor_sensor_tx, poll_period));
     }
 
     let (stop_button_tx, stop_button_rx) = cbc::unbounded::<bool>();
     {
         let elevator = elevator.clone();
-        spawn(move || elevio::poll::stop_button(elevator, stop_button_tx, poll_period));
+        spawn(move || poll::stop_button(elevator, stop_button_tx, poll_period));
     }
 
     let (obstruction_tx, obstruction_rx) = cbc::unbounded::<bool>();
     {
         let elevator = elevator.clone();
-        spawn(move || elevio::poll::obstruction(elevator, obstruction_tx, poll_period));
+        spawn(move || poll::obstruction(elevator, obstruction_tx, poll_period));
     }
 
-    let mut dirn = e::DIRN_DOWN;
+    let mut dirn = DIRN_DOWN;
 
 
     if elevator.floor_sensor().is_none() {
@@ -58,7 +57,8 @@ fn main() -> std::io::Result<()> {
                 let call_button = a.unwrap();
                 println!("{:#?}", call_button);
                 elevator.call_button_light(call_button.floor, call_button.call, true);
-                elevator.add_to_queue(call_button.floor);
+                let new_order = Order::init(call_button.floor,call_button.call);
+                elevator.add_to_queue(new_order);
                 if &elevator.status == &(Status::Idle){
                     elevator.go_next_floor();
                 }
@@ -90,7 +90,7 @@ fn main() -> std::io::Result<()> {
             recv(obstruction_rx) -> a => {
                 let obstr = a.unwrap();
                 println!("Obstruction: {:#?}", obstr);
-                elevator.motor_direction(if obstr { e::DIRN_STOP } else { dirn });
+                elevator.motor_direction(if obstr { DIRN_STOP } else { dirn });
                 
             },
         }
